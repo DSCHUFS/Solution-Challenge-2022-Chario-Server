4d1ca3318112a65598dd318b6288973100609462
DonghyunShin <shindh0429@gmail.com>
2022-02-03 03:02:02 +0900

    Initial Settings

---
+++ src/main/java/ChariO/GiBoo/GiBooApplication.java
@@ -0,0 +1,13 @@
+package ChariO.GiBoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiBooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiBooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ChariO/GiBoo/GiBooApplicationTests.java
@@ -0,0 +1,13 @@
+package ChariO.GiBoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiBooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
