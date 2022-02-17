0c9f83ee9fdf928fee7b6059a1fb5c782c471dc9
DonghyunShin <shindh0429@gmail.com>
2022-02-18 03:47:07 +0900

    Feat: Add UserService

---
+++ src/main/java/ChariO/GiBoo/service/UserService.java
@@ -0,0 +1,25 @@
+package ChariO.GiBoo.service;
+
+import ChariO.GiBoo.domain.User;
+import ChariO.GiBoo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public User findOne(Long userid){
+        return userRepository.findOne(userid);
+    }
+}
