e6589ccfa8c7a5f4542634ed295c422407228437
DonghyunShin <shindh0429@gmail.com>
2022-02-28 06:53:26 +0900

    Feat: Donation CRUD

--- src/main/java/ChariO/GiBoo/repository/DonRepository.java
+++ src/main/java/ChariO/GiBoo/repository/DonRepository.java
@@ -1,6 +1,9 @@
 package ChariO.GiBoo.repository;
 
 import ChariO.GiBoo.domain.Donation;
+import ChariO.GiBoo.domain.Facility;
+import ChariO.GiBoo.domain.Subscribe;
+import ChariO.GiBoo.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -15,6 +18,18 @@ public class DonRepository {
     @Autowired
     private EntityManager em;
 
+    public Donation create(Long u_id, String f_name, int price, String don_date) {
+        Donation donation = new Donation();
+        User user = em.find(User.class, u_id);
+        Facility facility = em.find(Facility.class, f_name);
+
+        donation.setUser(user);
+        donation.setFacility(facility);
+        donation.setDonationPrice(price);
+        donation.setDonationDate(don_date);
+        return donation;
+    }
+
     public List<Donation> userDonateAll(Long id) {
         return em.createQuery(" select d from Donation d " +
                 "join fetch d.facility f " +
@@ -24,4 +39,29 @@ public class DonRepository {
                 .getResultList();
     }
 
+    public Donation update(Long u_id, String f_name, int price, String don_date) {
+        Donation donation =
+                em.createQuery("select d from Donation d" +
+                " where d.user.id =: u_id " +
+                " and d.facility.f_name =: f_name", Donation.class)
+                .setParameter("u_id", u_id)
+                .setParameter("f_name", f_name).getSingleResult();
+
+        donation.setDonationPrice(price);
+        donation.setDonationDate(don_date);
+        return donation;
+    }
+
+    public Donation findByUserFac(Long u_id, String f_name){
+        return em.createQuery("select d from Donation d " +
+                        "where d.user.id =: u_id " +
+                        "and d.facility.f_name =: f_name ", Donation.class)
+                .setParameter("u_id", u_id)
+                .setParameter("f_name", f_name)
+                .getSingleResult();
+    }
+
+    public void delete(Donation donation){
+        em.remove(donation);
+    }
 }
