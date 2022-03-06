4c3d8b819f0d9466f1fb399cf2f21eb21a22d956
DonghyunShin <shindh0429@gmail.com>
2022-03-07 04:18:26 +0900

    Feat: 검색어 기반 기관 검색 기능 추가

--- src/main/java/ChariO/GiBoo/repository/FacRepository.java
+++ src/main/java/ChariO/GiBoo/repository/FacRepository.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -16,20 +17,27 @@ public class FacRepository {
     @Autowired
     private final EntityManager em;
 
-    public List<Facility> findAll(){
+    public List<Facility> findAll() {
         return em.createQuery("select f from Facility f", Facility.class)
                 .getResultList();
     }
 
-    public List<FacilityCategory> findByCategory(Long category_id){
-        return em.createQuery("select fc from FacilityCategory fc "+
-                "join fetch fc.facility f "+
-                "where fc.category.id = :category_id", FacilityCategory.class)
+    public List<FacilityCategory> findByCategory(Long category_id) {
+        return em.createQuery("select fc from FacilityCategory fc " +
+                        "join fetch fc.facility f " +
+                        "where fc.category.id = :category_id", FacilityCategory.class)
                 .setParameter("category_id", category_id)
                 .getResultList();
     }
 
-    public Facility findOne(Long id){
+    public Facility findOne(Long id) {
         return em.find(Facility.class, id);
+    }
+
+    //For Search
+    public Facility findOneByName(String facName) {
+        return em.createQuery(" select f from Facility f"+
+                " where f.f_name = :facName", Facility.class)
+                .setParameter("facName", facName).getSingleResult();
     }
 }
