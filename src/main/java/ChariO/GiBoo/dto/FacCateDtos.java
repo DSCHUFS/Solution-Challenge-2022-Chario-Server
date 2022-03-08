a5bc45636a6cbbda582e80a2b5106f784cd307b3
430lyj <430lyj@gmail.com>
2022-03-09 01:56:22 +0900

    feat: category 명 조회 api 개발

--- src/main/java/ChariO/GiBoo/dto/FacCateDtos.java
+++ src/main/java/ChariO/GiBoo/dto/FacCateDtos.java
@@ -4,6 +4,9 @@ import ChariO.GiBoo.domain.Facility;
 import ChariO.GiBoo.domain.FacilityCategory;
 import lombok.Data;
 
+import java.util.List;
+
+import static ChariO.GiBoo.dto.CateDtos.*;
 import static ChariO.GiBoo.dto.FacDtos.*;
 
 public class FacCateDtos {
@@ -23,6 +26,17 @@ public class FacCateDtos {
 
         public FacByCateDto(FacilityCategory fc){
             this.facility = new FacDto(fc.getFacility());
+        }
+    }
+
+    @Data
+    public static class GetCateResponse {
+        private Long count;
+        private List<CateDto> categories;
+
+        public GetCateResponse(List<CateDto> categories) {
+            this.count = categories.stream().count();
+            this.categories = categories;
         }
     }
 }
