89493cf78364a33c887b09c02230b751eb48aa23
DonghyunShin <shindh0429@gmail.com>
2022-03-12 23:24:37 +0900

    Chore: Swagger code 추가

--- src/main/java/ChariO/GiBoo/api/SearchApiController.java
+++ src/main/java/ChariO/GiBoo/api/SearchApiController.java
@@ -5,6 +5,9 @@ import ChariO.GiBoo.dto.FacDtos;
 import ChariO.GiBoo.dto.SearchDtos;
 import ChariO.GiBoo.dto.UserDtos;
 import ChariO.GiBoo.service.FacService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
@@ -23,10 +26,16 @@ public class SearchApiController {
     @Autowired
     private final FacService facService;
 
+    @Operation(summary = "검색 기능", description = "기관의 부분적인 이름만 입력해도 검색 가능")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping(value = "/api/search", produces = "application/json;charset=UTF-8")
     public SearchGetResponse2 search(@Param("facName") String facName){
 
-        //Facility facility = facService.findOneByName(request.getKeyword());
         List<Facility> facility1 = facService.findOneByNameLike(facName);
         List<FacDto> collect = facility1.stream()
                 .map(f -> new FacDto(f))
